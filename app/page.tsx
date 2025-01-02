9bb97c7df2ee0ab98efd8e3d12dcceafd2c5c5eb
DHARAKA METH <130528796+DHARAKA-METH@users.noreply.github.com>
2025-01-02 21:36:06 +0530

    ESlint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-white font-black">Hellow Next Js mastry</h1>
+    <h1 className="text-3xl font-black text-white">Hellow Next Js mastry</h1>
   );
 }
