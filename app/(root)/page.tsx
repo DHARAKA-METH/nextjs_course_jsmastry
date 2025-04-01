cbe1e85d20ad09abd29e83d08502be09465494b9
DHARAKA METH <130528796+DHARAKA-METH@users.noreply.github.com>
2025-04-01 14:38:15 +0530

    Implement Github SignIn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
-export default function Home() {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+export default async function Home() {
+  const session = await auth();
+  console.log(session);
   return (
     <>
       <h1 className="text-3xl font-black">Hellow Next Js mastry</h1>
       <h1 className="font-space-grotesk text-3xl font-black">
         Hellow Next Js mastry
       </h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit" className="">
+          Log Out
+        </Button>
+      </form>
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import type { Metadata } from "next";
 // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/toaster";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 const inter = Inter({
   subsets: ["latin"], // Specify the subsets you want
@@ -26,13 +29,17 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session =await auth()
+
   return (
     <html lang="en" suppressHydrationWarning>
+      <SessionProvider>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
@@ -44,7 +51,9 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+      <Toaster/>
       </body>
+      </SessionProvider>
     </html>
   );
 }
