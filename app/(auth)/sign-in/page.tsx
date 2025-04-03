5567c682a0a14a45b3054c836a3f712be09f58e8
DHARAKA METH <130528796+DHARAKA-METH@users.noreply.github.com>
2025-04-03 18:24:44 +0530

    Creating Auth Form UI

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import AuthForm from "@/components/form/AuthForm";
+import { SignInSchema } from "@/lib/validation";
 import React from "react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return <AuthForm
+  formType ="SIGN_IN"
+  schema={SignInSchema}
+  defaltValues={{email: "", password: ""}}
+  onSubmit={(data) =>Promise.resolve({success:true})}
+  />;
 };
 
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import AuthForm from "@/components/form/AuthForm";
+import { SignUpSchema } from "@/lib/validation";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaltValues={{ email: "", password: "",name:"",username:"" }}
+      onSubmit={(data) => Promise.resolve({ success: true })}
+    />
+  );
 };
 
 export default SignUp;
