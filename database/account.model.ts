d50be84c017e11f324a9cacc2d8e4213f916073b
DHARAKA METH <130528796+DHARAKA-METH@users.noreply.github.com>
2025-05-14 19:52:03 +0530

    format document

--- database/account.model.ts
+++ database/account.model.ts
@@ -1,49 +1,27 @@
-import mongoose, { Schema, Types } from "mongoose";
-
+import { Schema, model, models, Types, Document } from "mongoose";
 
 export interface IAccount {
   userId: Types.ObjectId;
   name: string;
   image?: string;
-  password?: string; 
-  provider: string;
+  password?: string;
+  provider: string; // e.g., 'github', 'google', 'credentials'
   providerAccountId: string;
-
 }
 
-
+export interface IAccountDoc extends IAccount, Document {}
 const AccountSchema = new Schema<IAccount>(
   {
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: "User", // Reference to User model
-      required: true,
-    },
-    name: {
-        type: String,
-      },
-    provider: {
-      type: String,
-      required: true,
-    },
-    providerAccountId: {
-      type: String,
-      required: true,
-      unique: true,// To avoid duplicates from the same provider
-    },
-    
-    image: {
-      type: String,
-    },
-    password: {
-      type: String, // Only used for credentials provider
-    },
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: String, required: true },
+    image: { type: String },
+    password: { type: String },
+    provider: { type: String, required: true },
+    providerAccountId: { type: String, required: true },
   },
-  {
-    timestamps: true, // Automatically adds createdAt and updatedAt
-  }
+  { timestamps: true },
 );
 
-// Export the model
-export default mongoose.models.Account ||
-  mongoose.model<IAccount>("Account", AccountSchema);
+const Account = models?.Account || model<IAccount>("Account", AccountSchema);
+
+export default Account;
--- database/user.model.ts
+++ database/user.model.ts
@@ -1,48 +1,31 @@
-import { model, models, Schema } from "mongoose";
+import { Schema, models, model, Document } from "mongoose";
 
 export interface IUser {
   name: string;
   username: string;
   email: string;
   bio?: string;
-  image: string;
+  image?: string;
   location?: string;
-  portfiolio?: string;
+  portfolio?: string;
   reputation?: number;
 }
 
-const userSchema = new Schema(
+export interface IUserDoc extends IUser, Document {}
+const UserSchema = new Schema<IUser>(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    username: { type: String, required: true },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    bio: {
-      type: String,
-    },
-
-    image: {
-      type: String,
-      required: false,
-    },
-    location: {
-      type: String,
-    },
-
-    portfiolio: {
-      type: String,
-    },
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String },
+    image: { type: String },
+    location: { type: String },
+    portfolio: { type: String },
     reputation: { type: Number, default: 0 },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
-const User = models?.user || model<IUser>("user", userSchema);
+const User = models?.User || model<IUser>("User", UserSchema);
+
 export default User;
