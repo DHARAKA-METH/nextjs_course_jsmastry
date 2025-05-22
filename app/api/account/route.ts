56b3726f30531abe65fc1db47f93496e0b1912f8
DHARAKA METH <130528796+DHARAKA-METH@users.noreply.github.com>
2025-05-22 22:48:34 +0530

    Create & Read Account Routes

---
+++ app/api/account/route.ts
@@ -0,0 +1,42 @@
+import Account from "@/database/account.model";
+import { handleError } from "@/lib/handlers/error";
+import { ForbiddenError } from "@/lib/http-errors";
+import dbconnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await dbconnect();
+    const account = await Account.find();
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbconnect();
+    const body = await request.json();
+    const validatedData = AccountSchema.parse(body);
+
+    const existingAccount = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccountId: validatedData.providerAccountId,
+    });
+
+    if (existingAccount) {
+      throw new ForbiddenError("Account with same provider already exists");
+    }
+
+    const newAccount = await Account.create(validatedData);
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
