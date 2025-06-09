74637aa247352e7a5c242d03ba3e8ec1ce0a33ac
DHARAKA METH <130528796+DHARAKA-METH@users.noreply.github.com>
2025-06-09 09:08:48 +0530

    Accounts for oAuth

---
+++ app/api/auth/signin-with-oauth/route.ts
@@ -0,0 +1,82 @@
+import Account from "@/database/account.model";
+import User from "@/database/user.model";
+import { handleError } from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
+import dbconnect from "@/lib/mongoose";
+import { SignInWithOAuthSchema } from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+import mongoose from "mongoose";
+import slugify from "slugify";
+
+export async function POST(request: Request) {
+  const { provider, providerAccountId, user } = await request.json();
+
+  await dbconnect();
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const validatedData = SignInWithOAuthSchema.safeParse({
+      provider,
+      providerAccountId,
+      user,
+    });
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const { name, username, email, image } = user;
+    const slugifiedUsername = slugify(username, {
+      lower: true,
+      strict: true,
+      trim: true,
+    });
+    let existinfUser = await User.findOne({ email }).session(session);
+    if (!existinfUser) {
+      [existinfUser] = await User.create(
+        [{ name, username: slugifiedUsername, email, image }],
+        { session }
+      );
+    } else {
+      const updatedData: { name?: string; image?: string } = {};
+      if (existinfUser.name !== name) {
+        updatedData.name = name;
+      }
+      if (existinfUser.image !== image) {
+        updatedData.image = image;
+      }
+      if (Object.keys(updatedData).length > 0) {
+        await User.updateOne({ email }, { $set: updatedData }).session(session);
+      }
+    }
+
+    const existingAccount = await Account.findOne({
+      userId: existinfUser._id,
+      provider,
+      providerAccountId,
+    }).session(session);
+
+    if (!existingAccount) {
+      await Account.create(
+        [
+          {
+            userId: existinfUser._id,
+            name,
+            image,
+            provider,
+            providerAccountId,
+          },
+        ],
+        { session }
+      );
+    }
+    await session.commitTransaction();
+  } catch (error: unknown) {
+    await session.abortTransaction();
+    return handleError(error, "api") as APIErrorResponse;
+  } finally {
+    session.endSession();
+  }
+}
