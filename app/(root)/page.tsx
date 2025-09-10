a9672ea1d3db415204c458f362a71402e212592f
DHARAKA METH <130528796+DHARAKA-METH@users.noreply.github.com>
2025-09-10 11:20:19 +0530

    implement generateMetadata function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -23,7 +23,7 @@ const Home = async ({ searchParams }: SearchParams) => {
 
   const { success, data, error } = await getQuestions({
     page: Number(page) || 1,
-    pageSize: Number(pageSize) || 2,
+    pageSize: Number(pageSize) || 10,
     query: query || "",
     filter: filter || "",
   });
